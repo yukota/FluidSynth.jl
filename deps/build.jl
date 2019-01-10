92c4d407102acd5f87dba8e6cbad500087d0507e
YuK_Ota <whitenightwinter@gmail.com>
2019-01-10 21:48:07 +0900

    feat: add osx dep target

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,13 @@ using BinDeps
 
 @BinDeps.setup
 
-fluidsynth = library_dependency("fluidsynth", aliases=["libfluidsynth.so"])
+fluidsynth = library_dependency("fluidsynth", aliases=["libfluidsynth"])
 
-provides(AptGet, Dict("libfluidsynth-dev" => fluidsynth))
+provides(AptGet, "libfluidsynth-dev", fluidsynth)
+
+@static if Sys.isapple()
+    using Homebrew
+    provides(Homebrew.HB, "fluid-synth", fluidsynth, os = :Darwin)
+end
 
 @BinDeps.install Dict(:fluidsynth => :libfluidsynth)
